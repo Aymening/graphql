815688008b311ba9c2d0750c16b27635227102e1
Aymen Hiti <aymenhiti24@gmail.com>
2025-02-25 12:43:29 +0000

    file structer is done

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (!username || !password) {
+        alert("Please enter username or email ");
+        return;
+    }
+    fetch("https://learn.zone01oujda.ma/api/auth/signin", {
+        method: "POST",
+        headers: {
+            "Authorization": `Basic ${btoa(`${username}:${password}`)}`,
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                alert('incorrect username or password!')
+                window.location.href = "index.html"
+                return Promise.reject('Invalid credentials'); //This stops  .then()  from running when the credentials are invalid
+            }
+        })
+        .then(data => {
+            localStorage.setItem("jwt", data);
+            window.location.href = "home.html"
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+}
+document.addEventListener("DOMContentLoaded", function () {
+    const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
+    usernameField.addEventListener("keydown", handleEnterKey);
+    passwordField.addEventListener("keydown", handleEnterKey);
+    function handleEnterKey(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            login();
+        }
+    }
+});
